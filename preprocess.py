efff7a39c21b8e6e74925c4342d58cb57b3138f6
mrityunjay5004 <mrityunjaytiwari5004@gmail.com>
2025-04-19 06:51:26 +0530

    Fix: use prebuilt spaCy + thinc wheels

--- preprocess.py
+++ preprocess.py
@@ -8,5 +8,5 @@ stop_words = set(stopwords.words('english'))
 nlp = spacy.load('en_core_web_sm', disable=["ner", "parser"])
 
 def spacy_preprocess(text):
-    doc = nlp(text.lower())
-    return " ".join([token.lemma_ for token in doc if token.is_alpha and token.text not in stop_words])
+    doc = nlp(text)
+    return " ".join([token.lemma_ for token in doc if not token.is_stop])
